62e4e815163e5c44b381d11e18f0fca79706b0ed
Aurélien St <aurelien.strimelle@bstorm.be>
2025-02-28 14:00:02 +0100

    Send player data 🌼

--- controllers/player.controller.js
+++ controllers/player.controller.js
@@ -1,7 +1,10 @@
+import playerModel from "../models/player.model.js";
+
 const playerController = {
 
     getAll: (req, res) => {
-        res.sendStatus(501);
+        const players = playerModel.find();
+        res.status(200).json(players);
     },
 
     add: (req, res) => {
