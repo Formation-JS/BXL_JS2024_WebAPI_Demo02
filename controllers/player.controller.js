0a5d278988700c451d85e3e3eacf05a869d1015c
Aurélien St <aurelien.strimelle@bstorm.be>
2025-02-28 14:16:19 +0100

    Using Pagination 🌹

--- controllers/player.controller.js
+++ controllers/player.controller.js
@@ -3,8 +3,13 @@ import playerModel from "../models/player.model.js";
 
 const playerController = {
 
+    //! /api/player?offset=2&limit=2
     getAll: (req, res) => {
-        const players = playerModel.find().map(p => new PlayerDTO(p));
+        const offset = parseInt(req.query.offset || 0);
+        const limit = parseInt(req.query.limit || 10);
+        
+        const players = playerModel.find({ offset, limit }).map(p => new PlayerDTO(p));
+
         res.status(200).json(players);
     },
 
