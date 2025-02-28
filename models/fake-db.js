eef1e480144b99ee1a74b707c2e39b2527cda788
Aurélien St <aurelien.strimelle@bstorm.be>
2025-02-28 11:41:36 +0100

    FakeDB 👽

---
+++ models/fake-db.js
@@ -0,0 +1,50 @@
+export const fakeDB = {
+    players: [
+        {
+            id: 1,
+            username: "DragonSlayer",
+            lastname: "Dupont",
+            firstname: "Jean",
+            email: "jean.dupont@example.com",
+            birthdate: "1990-04-12",
+            active: true
+        },
+        {
+            id: 2,
+            username: "ShadowHunter",
+            lastname: "Martin",
+            firstname: "Claire",
+            email: "claire.martin@example.com",
+            birthdate: null,
+            active: false
+        },
+        {
+            id: 4, 
+            username: "SpeedRacer",
+            lastname: null,
+            firstname: "Luc",
+            email: "speed.racer@example.com",
+            birthdate: "1995-01-15",
+            active: true
+        },
+        {
+            id: 8, 
+            username: "MysticMage",
+            lastname: "Leroy",
+            firstname: "Anna",
+            email: "anna.leroy@example.com",
+            birthdate: "1992-11-30",
+            active: true
+        },
+        {
+            id: 9,
+            username: "WarriorQueen",
+            lastname: "Moreau",
+            firstname: "Sophie",
+            email: "sophie.moreau@example.com",
+            birthdate: "1985-08-10",
+            active: false
+        }
+    ],
+    playerLastId: 9
+}
---
+++ models/player.model.js
@@ -0,0 +1,53 @@
+import { fakeDB } from "./fake-db.js";
+
+const playerModel = {
+
+    find: ({ offset = 0, limit = 3 }) => {
+        const players = fakeDB.players.slice(offset, offset + limit);
+        return structuredClone(players);
+    },
+
+    findFilter: (conditionCallback, { offset = 0, limit = 3 }) => {
+        const players = fakeDB.players.filter(conditionCallback)
+                                      .slice(offset, offset + limit);
+        return structuredClone(players);
+    },
+
+    findById: (id) => {
+        const player = fakeDB.players.find(p => p.id === id);
+        return structuredClone(player);
+    },
+
+    insert: ({ username, lastname, firstname, email, birthdate }) => {
+        if(!username?.trim() || !email?.trim()) {
+            throw new Error ('Error on insert player');
+        };
+
+        fakeDB.playerLastId++;
+        const playerAdded = {
+            id: fakeDB.playerLastId,
+            username, lastname, firstname, email, birthdate,
+            active: true
+        };
+        fakeDB.players.push(playerAdded);
+        
+        return structuredClone(playerAdded);
+    },
+
+    modify: (id, { username, lastname, firstname, email, birthdate }) => {
+        throw new Error('Not implemented');
+    },
+
+    remove: (id) => {
+        const playerIndex = fakeDB.players.findIndex(player => player.id === id);
+       
+        if(playerIndex === -1) {
+            return false;
+        }
+
+        fakeDB.players.splice(playerIndex, 1);
+        return true;
+    }
+
+}
+export default playerModel;
