cc16230a36cf1b0c80e3d07250e04f8203f1c170
Aurélien St <aurelien.strimelle@bstorm.be>
2025-03-03 15:49:59 +0100

    Manage boolean active 🐺

--- models/player.model.js
+++ models/player.model.js
@@ -1,21 +1,27 @@
 import { fakeDB } from "./fake-db.js";
 
 const defaultOptions = {};
+
+
+function getActivePlayers(byPass = false) {
+    return fakeDB.players.filter(player => byPass || player.active);
+}
+
 const playerModel = {
 
-    find: ({ offset = 0, limit = 3 } = defaultOptions) => {
-        const players = fakeDB.players.slice(offset, offset + limit);
+    find: ({ offset = 0, limit = 3, byPassActive = false } = defaultOptions) => {
+        const players = getPlayers(byPassActive).slice(offset, offset + limit);
         return structuredClone(players);
     },
 
-    findFilter: (conditionCallback, { offset = 0, limit = 3 } = defaultOptions) => {
-        const players = fakeDB.players.filter(conditionCallback)
-                                      .slice(offset, offset + limit);
+    findFilter: (conditionCallback, { offset = 0, limit = 3, byPassActive = false  } = defaultOptions) => {
+        const players = getActivePlayers(byPassActive).filter(conditionCallback)
+                                                      .slice(offset, offset + limit);
         return structuredClone(players);
     },
 
-    findById: (id) => {
-        const player = fakeDB.players.find(p => p.id === id);
+    findById: (id, byPassActive = false) => {
+        const player = getActivePlayers(byPassActive).find(p => p.id === id);
         return structuredClone(player);
     },
 
@@ -39,15 +45,27 @@ const playerModel = {
         throw new Error('Not implemented');
     },
 
-    remove: (id) => {
+    remove: (id, forceRemove = false) => {
         const playerIndex = fakeDB.players.findIndex(player => player.id === id);
        
         if(playerIndex === -1) {
             return false;
         }
 
-        fakeDB.players.splice(playerIndex, 1);
-        return true;
+        const player = fakeDB.players[playerIndex];
+
+        if(player.active) {
+            // Soft delete
+            player.active = false;
+            return true;
+        }
+        else if(forceRemove) {
+            // True delete
+            fakeDB.players.splice(playerIndex, 1);
+            return true;
+        }
+
+        return false;
     }
 
 }
